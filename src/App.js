b72aac835edbdcb0b10dd7dd53ce381f4be43ff1
Paweł Grajewski <unionproject88@gmail.com>
2020-10-06 22:24:34 +0200

    basic example of redux

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-import React from "react";
-import "./styles.css";
+import React, { Component } from "react";
 
-export default function App() {
-  return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
-  );
+import Counter from "./containers/Counter/Counter";
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Counter />
+      </div>
+    );
+  }
 }
+
+export default App;
