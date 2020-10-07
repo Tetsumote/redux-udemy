1e7c2de636a618abc28dbd663c7990eaf504c1cc
Paweł Grajewski <unionproject88@gmail.com>
2020-10-07 23:16:07 +0200

    add redux basic concept

--- redux-basics.js
+++ redux-basics.js
@@ -26,6 +26,7 @@ const rootReducer = (state = initialState, action) => {
 const store = createStore(rootReducer);
 console.log(store.getState());
 
+// Subscription
 store.subscribe(() => {
   console.log("[Subscription]", store.getState());
 });
@@ -34,5 +35,3 @@ store.subscribe(() => {
 store.dispatch({ type: "INC_COUNTER" });
 store.dispatch({ type: "ADD_COUNTER", value: 10 });
 console.log(store.getState());
-
-// Subscription
