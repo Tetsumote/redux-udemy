b9908cecbb69a436b688f4074ea4c82e06bf875e
Paweł Grajewski <unionproject88@gmail.com>
2020-10-08 23:15:58 +0200

    add dispatch for another events

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -8,6 +8,21 @@ const reducer = (state = initialState, action) => {
       counter: state.counter + 1
     };
   }
+  if (action.type === "DECREMENT") {
+    return {
+      counter: state.counter - 1
+    };
+  }
+  if (action.type === "ADD") {
+    return {
+      counter: state.counter + 11
+    };
+  }
+  if (action.type === "SUBTRACT") {
+    return {
+      counter: state.counter - 8
+    };
+  }
   return state;
 };
 
