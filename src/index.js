81f3255fd28539e1b3cdfd6bc7bdb09d81b3f128
Paweł Grajewski <unionproject88@gmail.com>
2020-10-10 20:42:14 +0200

    add reduc and two reducers

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { createStore } from "redux";
+import { createStore, combineReducers } from "redux";
+
 import reducer from "./store/reducer";
+import counterReducer from "./store/reducers/counter";
+import resultReducer from "./store/reducers/result";
+
 import { Provider } from "react-redux";
 
 import App from "./App";
 
-const store = createStore(reducer);
+const rootReducer = combineReducers({
+  ctr: counterReducer,
+  res: resultReducer
+});
+
+const store = createStore(rootReducer);
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(
