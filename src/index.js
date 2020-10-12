43930b56c71ffafbcc3b58c2796beda2981c280b
Paweł Grajewski <unionproject88@gmail.com>
2020-10-12 23:17:32 +0200

    debug in devtools

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { createStore, combineReducers } from "redux";
+import { createStore, combineReducers, applyMiddleware, compose } from "redux";
 
 import reducer from "./store/reducer";
 import counterReducer from "./store/reducers/counter";
@@ -15,7 +15,23 @@ const rootReducer = combineReducers({
   res: resultReducer
 });
 
-const store = createStore(rootReducer);
+const logger = (store) => {
+  return (next) => {
+    return (action) => {
+      console.log("[Middleware] Dispatching", action);
+      const result = next(action);
+      console.log("Middleware next state", store.getState());
+      return result;
+    };
+  };
+};
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(
+  rootReducer,
+  composeEnhancers(applyMiddleware(logger))
+);
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(
