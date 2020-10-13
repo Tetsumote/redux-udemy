91afb5cd6c22674709580cda8333249f17cdb94e
Paweł Grajewski <unionproject88@gmail.com>
2020-10-13 23:48:51 +0200

    add async reducer

---
+++ src/store/actions/result.js
@@ -0,0 +1,31 @@
+import * as actionTypes from "./actionsTypes";
+
+export const saveResult = (res) => {
+  // const updatedResult = res * 2;
+  return {
+    type: actionTypes.STORE_RESULT,
+    result: res
+    // result: updatedResult
+  };
+};
+
+export const storeResult = (res) => {
+  return (dispatch, getState) => {
+    setTimeout(() => {
+      const oldCounter = getState().ctr.counter;
+      console.log(oldCounter);
+      dispatch(saveResult(res));
+    }, 2000);
+  };
+
+  // return {
+  //   type: STORE_RESULT,
+  //   result: res
+  // };
+};
+export const deleteResult = (resElId) => {
+  return {
+    type: actionTypes.DELETE_RESULT,
+    resultElId: resElId
+  };
+};
